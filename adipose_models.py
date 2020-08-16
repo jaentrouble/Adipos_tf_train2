fc7a044cfeed1e9713717f8506ae1fa30ff2f200
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2020-08-16 17:22:57 +0900

    model, lr update

---
+++ adipose_models.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import tensorflow.keras as keras
+from tensorflow.keras import layers
+
+# Get inputs and return outputs
+# Don't forget to squeeze output
+
+def conv4_b2_0(inputs):
+    x = layers.Conv2D(32, 3, padding='same', activation='relu')(inputs)
+    x = layers.Conv2D(32, 3, padding='same', activation='relu')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Conv2D(32, 3, padding='same', activation='relu')(x)
+    x = layers.Conv2D(32, 3, padding='same', activation='relu')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Conv2D(1, 3, padding='same', activation='linear')(x)
+    x = tf.squeeze(x, axis=-1)
+    outputs = layers.Activation('linear', dtype='float32')(x)
+    return outputs
---
+++ model_lr.py
@@ -0,0 +1,40 @@
+def lr_no_update(epoch, lr):
+    return 0.0
+
+def lr_step(epoch, lr):
+    lr = 0.2
+    if epoch > 10 :
+        lr = 0.02
+    if epoch > 20 :
+        lr = 0.01
+    if epoch > 50 :
+        lr = 0.005
+    return lr
+
+def lr_step2(epoch, lr):
+    if epoch <= 10 :
+        lr = (epoch +1) * 0.01
+    if epoch > 10 :
+        lr = 0.02
+    if epoch > 20 :
+        lr = 0.01
+    if epoch > 50 :
+        lr = 0.005
+    return lr
+
+def lr_step3(epoch, lr):
+    if epoch <= 10 :
+        lr = (epoch +1) * 0.01
+    if epoch > 10 :
+        lr = 0.02
+    if epoch > 20 :
+        lr = 0.01
+    if epoch > 50 :
+        lr = lr * 0.95
+    return lr
+
+def lr_mul_inv(epoch, lr):
+    return 0.01 / (epoch+1)
+
+def low_lr(epoch, lr) :
+    return 1e-5
