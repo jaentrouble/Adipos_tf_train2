e4aaecb96562e2c91bfe10a2426673d3f7e6c497
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2020-08-16 17:10:53 +0900

    model_trainer

--- model_trainer.py
+++ model_trainer.py
@@ -1,1 +1,88 @@
 import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.mixed_precision import experimental as mixed_precision
+import time
+
+class AdiposeModel(keras.Model):
+    def __init__(self, inputs, model_function):
+        """
+        Because of numerical stability, softmax layer should be
+        taken out, and use it only when not training.
+        Args
+            inputs : keras.Input
+            model_function : function that takes keras.Input and returns
+            output tensor of logits
+        """
+        super().__init__()
+        outputs = model_function(inputs)
+        self.logits = keras.Model(inputs=inputs, outputs=outputs)
+        self.logits.summary()
+        
+    def call(self, inputs, training=None):
+        casted = tf.cast(inputs, tf.float32) / 255.0
+        if training:
+            return self.logits(inputs, training=training)
+        return tf.math.sigmoid(self.logits(inputs, training=training))
+
+def lr_no_update(*args):
+    return 0.0
+
+def run_training(
+        model_f, 
+        lr_f, 
+        name, 
+        epochs, 
+        batch_size, 
+        X_train, 
+        Y_train, 
+        val_data,
+    ):
+    """
+    val_data : (X_val, Y_val) tuple
+    """
+    policy = mixed_precision.Policy('mixed_float16')
+    mixed_precision.set_policy(policy)
+    
+    st = time.time()
+
+    inputs = keras.Input((200,200,3))
+    mymodel = AdiposeModel(inputs, model_f)
+    loss = keras.losses.BinaryCrossentropy(from_logits=True)
+    mymodel.compile(
+        optimizer='adam',
+        loss=loss,
+        metrics=[
+            keras.metrics.BinaryAccuracy(threshold=0.1),
+        ]
+    )
+
+    logdir = 'logs/fit/' + name
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(
+        logdir=logdir,
+        histogram_freq=1,
+        profile_batch='3,5',
+        update_freq='epoch'
+    )
+    lr_callback = keras.callbacks.LearningRateScheduler(lr_f, verbose=1)
+    lr_no_update = keras.callbacks.LearningRateScheduler(lr_no_update, verbose=1)
+
+    savedir = 'savedmodels/' + name + '/{epoch}'
+    save_callback = keras.callbacks.ModelCheckpoint(
+        savedir,
+        verbose=1
+    )
+
+    mymodel.fit(
+        x=X_train,
+        y=Y_train,
+        epochs=epochs,
+        batch_size=batch_size,
+        callbacks=[
+            tensorboard_callback,
+            lr_callback,
+            save_callback,
+        ],
+        validation_data=val_data
+    )
+
+    print('Took {} seconds'.format(time.time()-st))
