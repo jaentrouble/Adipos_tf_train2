407106622210585d528b12dc7099756b3263ec0c
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2020-08-17 00:07:42 +0900

    why not saved??

--- model_trainer.py
+++ model_trainer.py
@@ -63,7 +63,7 @@ def run_training(
     )
     lr_callback = keras.callbacks.LearningRateScheduler(lr_f, verbose=1)
 
-    savedir = 'savedmodels/' + name + '{epoch}/{epoch}.hdf5'
+    savedir = 'savedmodels/' + name + '/{epoch}/{epoch}'
     save_callback = keras.callbacks.ModelCheckpoint(
         savedir,
         verbose=1
