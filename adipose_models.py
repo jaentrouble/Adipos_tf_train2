1197864b3cb072628a9ebbeb507df7c30d55a748
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2020-08-17 16:34:18 +0900

    BN layer change

--- adipose_models.py
+++ adipose_models.py
@@ -34,3 +34,25 @@ def conv16_b4_0(inputs):
     x = tf.squeeze(x, axis=-1)
     outputs = layers.Activation('linear', dtype='float32')(x)
     return outputs
+
+def conv16_b4_1(inputs):
+    x = layers.Conv2D(32, 3, padding='same', activation='relu')(inputs)
+    x = layers.Conv2D(32, 3, padding='same', activation='linear')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.ReLU()(x)
+    x = layers.Conv2D(32, 3, padding='same', activation='relu')(x)
+    x = layers.Conv2D(32, 3, padding='same', activation='linear')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.ReLU()(x)
+    x = layers.Conv2D(32, 3, padding='same', activation='relu')(x)
+    x = layers.Conv2D(32, 3, padding='same', activation='linear')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.ReLU()(x)
+    x = layers.Conv2D(32, 3, padding='same', activation='relu')(x)
+    x = layers.Conv2D(32, 3, padding='same', activation='linear')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.ReLU()(x)
+    x = layers.Conv2D(1, 3, padding='same', activation='linear')(x)
+    x = tf.squeeze(x, axis=-1)
+    outputs = layers.Activation('linear', dtype='float32')(x)
+    return outputs
